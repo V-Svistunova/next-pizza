172dbbb921c0925567577cfc081bfe48c127226f
Vika Svistunova <wika1706@gmail.com>
2024-10-17 17:29:43 +0300

    add sort and filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
+import { Container, Filters, Title, TopBar } from "@/components/shared";
+
 export default function Home() {
-  return (
-      <h1>Home</h1>
-  );
+  return <>
+
+    <Container className="mt-10">
+      <Title text="Все пиццы" size="lg" className="font-extrabold" />
+    </Container>
+
+    <TopBar/>
+
+    <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              Список товаров
+              {/* <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+              <ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]} /> */}
+            </div>
+
+            <div className="flex items-center gap-6 mt-12">
+              {/* <Pagination pageCount={3} /> */}
+              <span className="text-sm text-gray-400">5 из 65</span>
+            </div>
+          </div>
+        </div>
+      </Container>
+  </>;
 }
